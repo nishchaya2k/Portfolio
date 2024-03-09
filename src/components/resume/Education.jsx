a98a090d9f66714b3fa8fda1eb8498f61648f6e4
Nishchaya Narula <58116972+nishchaya2k@users.noreply.github.com>
2024-03-09 22:20:54 +0530

    Update Education.jsx
    Add Experience

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -4,16 +4,31 @@ const Education = () => {
     return (
         <div className='lg:grid grid-cols-2 gap-8 px-12 mt-8'>
             <div>
-                <h3 className="text-xl font-bold mb-8 text-primary">Experience</h3>
-                <div className='flex max-md:hidden items-center gap-4 text-sm '>
-                    <p className="px-3 py-1 border-2 rounded-full border-[#54CA95] ">Feb 2022 - Aug 2023</p>
-                    <p className="text-[#54CA95] font-semibold text-base">Srijan / A Material+ Company</p>
+                <h3 className="text-xl font-bold mb-8 text-primary">Experience (2 yrs)</h3>
+                <div>
+                    <div className='flex max-md:hidden items-center gap-4 text-sm '>
+                        <p className="px-3 py-1 border-2 rounded-full border-[#54CA95] ">December 2023 - March 2024</p>
+                        <p className="text-[#54CA95] font-semibold text-base">VeriTech Software IT Services</p>
+                    </div>
+                    <div className="text-lg text-gray-600 font-semibold border-l-[1px] border-b-[1px] mt-4 pl-5 pb-5 border-[#edebeb86]">
+                        <p>ReactJs Intern</p>
+                    </div>
                 </div>
-                <div className="text-lg text-gray-600 font-semibold border-l-[1px] border-b-[1px] mt-4 pl-5 pb-5 border-[#edebeb86]">
-                    <p>Developer L1, <span className='text-xs text-gray-500 font-medium'>(Sept 2022 - Aug 2023)</span></p>
-                    <p>JavaScript Developer <span className='text-xs text-gray-500 font-medium'>(Feb 2022 - Aug 2022)</span></p>
-                    {/* <p>Followed my passion for technology and shaping my skills in computer science through comprehensive academic programs and practical learning experiences</p> */}
+                <div>
+                    <div className='flex max-md:hidden items-center gap-4 text-sm '>
+                        <p className="px-3 py-1 border-2 rounded-full border-[#54CA95] ">Feb 2022 - Aug 2023</p>
+                        <p className="text-[#54CA95] font-semibold text-base">Srijan / A Material+ Company</p>
+                    </div>
+                    <div className="text-lg text-gray-600 font-semibold border-l-[1px] border-b-[1px] mt-4 pl-5 pb-5 border-[#edebeb86]">
+                        <p>Developer L1, <span className='text-xs text-gray-500 font-medium'>(Sept 2022 - Aug 2023)</span></p>
+                        <p>JavaScript Developer <span className='text-xs text-gray-500 font-medium'>(Feb 2022 - Aug 2022)</span></p>
+                        {/* <p>Followed my passion for technology and shaping my skills in computer science through comprehensive academic programs and practical learning experiences</p> */}
+                    </div>
                 </div>
+
+
+
+
             </div>
 
             <div>
